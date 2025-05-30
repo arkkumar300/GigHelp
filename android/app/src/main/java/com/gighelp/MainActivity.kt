d5a4c42bfa96bd7e0d2728e895545fd7905ca331
ravi kumar <ark.kumar03@gmail.com>
2025-05-30 18:40:52 +0530

    rrr

--- android/app/src/main/java/com/gighelp/MainActivity.kt
+++ android/app/src/main/java/com/gighelp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.gighelp
-
+import android.os.Build
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -12,7 +13,16 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "GigHelp"
+  override fun onCreate(savedInstanceState: Bundle?) {
+  //    SplashScreen.show(this);  // here
+        super.onCreate(null)
 
+//      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//          startForegroundService(Intent(this, LocationService::class.java))
+//      } else {
+//          startService(Intent(this, LocationService::class.java))
+//      }
+    }
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
